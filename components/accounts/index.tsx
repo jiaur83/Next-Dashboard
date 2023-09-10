05173b670942251bfaf580b3ff8c68524c98ef41
Jiaur <jiaur883@gmail.com>
2023-09-11 00:10:00 +0600

    Updated-01

--- components/accounts/index.tsx
+++ components/accounts/index.tsx
@@ -1,60 +1,50 @@
-import {Button, Input, Text} from '@nextui-org/react';
-import Link from 'next/link';
-import React from 'react';
-import {Breadcrumbs, Crumb, CrumbLink} from '../breadcrumb/breadcrumb.styled';
-import {DotsIcon} from '../icons/accounts/dots-icon';
-import {ExportIcon} from '../icons/accounts/export-icon';
-import {InfoIcon} from '../icons/accounts/info-icon';
-import {TrashIcon} from '../icons/accounts/trash-icon';
-import {HouseIcon} from '../icons/breadcrumb/house-icon';
-import {UsersIcon} from '../icons/breadcrumb/users-icon';
-import {SettingsIcon} from '../icons/sidebar/settings-icon';
-import {Flex} from '../styles/flex';
-import {TableWrapper} from '../table/table';
+import { Button, Input, Text } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import { Breadcrumbs, Crumb, CrumbLink } from "../breadcrumb/breadcrumb.styled";
+import { DotsIcon } from "../icons/accounts/dots-icon";
+import { ExportIcon } from "../icons/accounts/export-icon";
+import { InfoIcon } from "../icons/accounts/info-icon";
+import { TrashIcon } from "../icons/accounts/trash-icon";
+import { HouseIcon } from "../icons/breadcrumb/house-icon";
+import { UsersIcon } from "../icons/breadcrumb/users-icon";
+import { SettingsIcon } from "../icons/sidebar/settings-icon";
+import { Flex } from "../styles/flex";
+import { TableWrapper } from "../table/table";
 
 export const Accounts = () => {
-   return (
-      <Flex
-         css={{
-            'mt': '$5',
-            'px': '$6',
-            '@sm': {
-               mt: '$10',
-               px: '$16',
-            },
-         }}
-         justify={'center'}
-         direction={'column'}
-      >
-         <Text h3>All Accounts</Text>
-         <Flex
-            css={{gap: '$8'}}
-            align={'center'}
-            justify={'between'}
-            wrap={'wrap'}
-         >
-            <Flex
-               css={{
-                  'gap': '$6',
-                  'flexWrap': 'wrap',
-                  '@sm': {flexWrap: 'nowrap'},
-               }}
-               align={'center'}
-            >
-               <Input
-                  css={{width: '100%', maxW: '410px'}}
-                  placeholder="Search users"
-               />
-               <SettingsIcon />
-               <TrashIcon />
-               <InfoIcon />
-               <DotsIcon />
+    return (
+        <Flex
+            css={{
+                mt: "$5",
+                px: "$6",
+                "@sm": {
+                    mt: "$10",
+                    px: "$16",
+                },
+            }}
+            justify={"center"}
+            direction={"column"}
+        >
+            <Text h3>All Accounts</Text>
+            <Flex css={{ gap: "$8" }} align={"center"} justify={"between"} wrap={"wrap"}>
+                <Flex
+                    css={{
+                        gap: "$6",
+                        flexWrap: "wrap",
+                        "@sm": { flexWrap: "nowrap" },
+                    }}
+                    align={"center"}
+                >
+                    <Input css={{ width: "100%", maxW: "410px" }} placeholder="Search users" />
+                    <SettingsIcon />
+                    <TrashIcon />
+                    <InfoIcon />
+                    <DotsIcon />
+                </Flex>
             </Flex>
 
-         </Flex>
-
-         <TableWrapper />
-         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, nam ipsa! Architecto, facere, aliquid accusamus quis quae eaque necessitatibus blanditiis pariatur quam quod eius dolor quasi possimus? Nesciunt, molestias recusandae?</p>
-      </Flex>
-   );
+            <TableWrapper />
+        </Flex>
+    );
 };
